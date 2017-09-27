3874781a53a24946d3118c4383fd9182c892439e
Alejandro Gonzalez Rodrigo <alejandrogr@gmail.com>
2017-09-27 11:22:22 +0200

    add dal package

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from pip.download import PipSession
+from pip.req import parse_requirements
 from setuptools import setup, find_packages
 from os import path
 
@@ -8,6 +10,10 @@ here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, 'README.rst')) as f:
     readme = f.read()
+
+requirements = [str(ir.req) for ir in parse_requirements('requirements.txt', session=PipSession())]
+
+test_requirements = [str(ir.req) for ir in parse_requirements('requirements_test.txt', session=PipSession())]
 
 
 setup(
@@ -18,8 +24,11 @@ setup(
     long_description=readme,
     packages=find_packages(),
     keywords='deeptracy core',
+    install_requires=requirements,
     classifiers=[
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules'
-    ]
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
 )
