b59d6a69cc3c1a065f5a1e207eeef1257013ddea
Alejandro Gonzalez Rodrigo <alejandrogr@gmail.com>
2017-09-28 19:19:40 +0200

    remove custom exception

--- deeptracy_core/exceptions.py
+++
@@ -1,9 +0,0 @@
-
-class DeeptracyException(Exception):
-
-    def __init__(self, message, status_code=None):
-        # Call the base class constructor with the parameters it needs
-        super().__init__(message)
-
-        # Now for your custom code...
-        self.api_status_code = status_code
