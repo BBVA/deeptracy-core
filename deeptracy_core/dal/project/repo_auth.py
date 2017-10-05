1c9dc4c96ab4d9c82a21d9502d7173e3caba7197
Alejandro Gonzalez Rodrigo <alejandrogr@gmail.com>
2017-10-05 14:23:22 +0200

    comments on RepoAuth module

--- deeptracy_core/dal/project/repo_auth.py
+++ deeptracy_core/dal/project/repo_auth.py
@@ -4,7 +4,9 @@ This classes are related to repository authorization inside a project
 
 Attending to the RepoAuthType, repositories can be:
     RepoAuthType.PUBLIC: a public repository doesn't need authorization to pull
-    RepoAuthType.PRIVATE_KEY: private key repository needs a private key to be able to pull code
+    RepoAuthType.LOCAL_PRIVATE_KEY: private key repository needs a private key to be able to pull code, and the private
+        key is located in the host machine
+    RepoAuthType.USER_PWD: the repo needs a user and password to authenticate to be able to pull
 
 RepoAuth holds auth information for specific RepoAuthType. Object of type RepoAuth are intende
 """
