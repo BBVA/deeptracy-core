afbb1d890bd4ff117eb2f231e7ef10d78ef983e9
sergiodfdez <sergiodfdez@gmail.com>
2017-11-28 09:58:04 +0100

    Bump version: 0.0.25 → 0.0.26

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements = [str(ir.req) for ir in parse_requirements(
 
 setup(
     name='deeptracy_core',
-    version='0.0.25',
+    version='0.0.26',
     author='BBVA',
     url="https://github.com/BBVA/deeptracy-core",
     description='Deeptracy Dependency Checker',
